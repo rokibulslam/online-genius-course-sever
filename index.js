20d02f0f4925dafdfd19f70bbcc58a34461fba00
rokibulslam <rokibulslam6@gmail.com>
2022-02-18 14:24:51 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
       const database = client.db("onlineCourses");
       const courseCollection = database.collection("courses");
       const orderCollection = database.collection("orders");
-
+      const reviewCollection = database.collection("reviews");
       // GET METHOD
       // GET ALL COURSES
       app.get("/courses", async (req, res) => {
@@ -47,7 +47,22 @@ async function run() {
         const orders = await cursor.toArray();
         res.json(orders);
       });
-
+      // GET SPECIFIC USERS ORDER BY EMAIL
+      app.get("/orders/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = {
+          email: email,
+        };
+        const result = await orderCollection.find(query).toArray();
+        res.json(result);
+      });
+      // GET ALL REVIEWS
+      app.get("/reviews", async (req, res) => {
+        const cursor = reviewCollection.find({});
+        const reviews = await cursor.toArray();
+        res.send(reviews);
+      });
+      // ----------**-----------
       // POST METHOD
       //   ADD AN APARTMENT APARTMENT COLLECTION
       app.post("/courses", async (req, res) => {
@@ -56,10 +71,48 @@ async function run() {
         const result = await courseCollection.insertOne(course);
         res.json(result);
       });
-      // POST AN ORDER  
+      // POST AN ORDER
       app.post("/orders", async (req, res) => {
         const apartment = req.body;
         const result = await orderCollection.insertOne(apartment);
+        res.json(result);
+      });
+      // ADD REVIEW
+      app.post("/review", async (req, res) => {
+        const review = req.body;
+        const result = await reviewCollection.insertOne(review);
+        res.json(result);
+      });
+
+      // ----------**-----------
+      // UPDATE METHOD
+      // UPDATE ORDER'S STATUS BY ID
+      app.put("/order/status/:id", async (req, res) => {
+        const id = req.params.id;
+        const updateInfo = req.body;
+        const result = await orderCollection.updateOne(
+          { _id: ObjectId(id) },
+          { $set: { status: updateInfo.status } }
+        );
+        res.send(result);
+      });
+
+      // ----------**-----------
+      // DELETE METHOD
+      //   DELETE A COURSE FROM COLLECTION
+      app.delete("/course/delete/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await courseCollection.deleteOne(query);
+        res.json(result);
+      });
+      // DELETE AN ORDER
+      app.delete("/order/delete/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        console.log(result);
         res.json(result);
       });
     } finally  {
